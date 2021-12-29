4764efd92b8b60fbfb4b0e6ab1f677ba7a9823a4
Yogeshwari <yogeshwarishahir43@gmail.com>
2021-12-29 17:26:41 +0700

    first commit

---
+++ Smaller of two number/src/smallernum/Smallernum.java
@@ -0,0 +1,15 @@
+package smallernum;
+
+public class Smallernum {
+	 public static void main(String[] args) {
+		
+			 int a=20,b=40;
+			    if (a<b) {
+			    	System.out.println("A is smaller");
+			    	
+			    }else {
+			    	System.out.println("b is smaller");
+			    }
+			 
+		  }
+}
