668ccfc9db46ffb206b79f066cf78f9e9ac61c13
Yogeshwari <yogeshwarishahir43@gmail.com>
2021-12-29 17:43:32 +0700

    second commit

---
+++ Ascii value/src/asciivalue/Asciivalue.java
@@ -0,0 +1,13 @@
+package asciivalue;
+
+public class Asciivalue {
+ public static void main(String[] args) {
+	 int i;
+	 i=0;
+	 while(i<=255) {
+		 System.out.println("Ascii values are :- "+i);
+		 i++;
+		 
+	 }
+ }
+}
