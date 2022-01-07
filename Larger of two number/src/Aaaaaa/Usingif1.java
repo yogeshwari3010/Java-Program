132ab0f3e40e73a373b80ca2aff93b32694b669d
Yogeshwari <yogeshwarishahir43@gmail.com>
2022-01-07 17:10:23 +0700

    Larger  of two numbers in java

---
+++ Larger of two number/src/Aaaaaa/Usingif1.java
@@ -0,0 +1,19 @@
+package Aaaaaa;
+
+import java.util.Scanner;
+
+public class Usingif1 {
+	 public static void main(String[] args) {
+		//  Scanner sc=new Scanner (System.in);
+		 // System.out.println("");
+		 //  boolean per=sc.nextBoolean();
+		 int a=20,b=40;
+		    if (a>b) {
+		    	System.out.println("A is greater");
+		    	
+		    }else {
+		    	System.out.println("b is greater");
+		    }
+		   // sc.close();
+	  }
+}
---
+++ Larger of two number/src/Aaaaaa/usingifelse.java
@@ -0,0 +1,15 @@
+package Aaaaaa;
+
+public class usingifelse {
+   public static void main(String[] args) {
+	   
+    int a=20,b=40;
+	    if (a<b) {
+	    	System.out.println("B is greater");
+	    	
+	    }else {
+	    	System.out.println("A is greater");
+	    }
+}
+
+}
