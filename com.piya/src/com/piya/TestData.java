a8b5180e6d4153aac9ac59ed0d98c746acb7fedd
Yogeshwari <yogeshwarishahir43@gmail.com>
2022-01-07 16:35:10 +0700

    Do While Loop example in java

---
+++ com.piya/src/com/piya/TestData.java
@@ -0,0 +1,16 @@
+package com.piya;
+
+public class TestData {
+	//public String toString() {
+	//return"hii"	;
+	//}
+   public static void main(String[] args) {
+	   Data d=new Data();
+	 
+	//   System.out.println();
+	   System.out.println(d.hashCode());
+	   System.out.println(d.getClass());
+	   System.out.println(d);
+	  // System.out.println(d.);
+   }
+}
