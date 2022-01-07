69c5e4f474b7863f8931b2168dc3fd2200d83add
Yogeshwari <yogeshwarishahir43@gmail.com>
2022-01-07 17:24:37 +0700

    Method Example in java

---
+++ Method3/src/kkkkkk/Piya.java
@@ -0,0 +1,8 @@
+package kkkkkk;
+
+public class Piya {
+	public int age;
+	public String nature;
+	public double height;
+
+}
---
+++ Method3/src/kkkkkk/Piya2.java
@@ -0,0 +1,15 @@
+package kkkkkk;
+
+public class Piya2 {
+	public static void main(String[] args) {
+		Piya p=new Piya();
+		p.age=20;
+		p.nature="simple";
+		p.height=5.56;
+		System.out.println("Age = "+p.age);
+		System.out.println("nature = "+p.nature);
+		System.out.println("height = "+p.height);
+		
+	}
+
+}
