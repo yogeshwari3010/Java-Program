82f634c3c858b1fa1e081fa185905f2a3b10079b
Yogeshwari <yogeshwarishahir43@gmail.com>
2022-01-07 17:06:15 +0700

    Introduction to array in java

---
+++ Intro to array/src/array/Array.java
@@ -0,0 +1,12 @@
+package array;
+
+public class Array {
+ public static void main(String[] args) {
+	 int a[] = {11, 22, 33, 44,55};
+	 int i;
+	 for(i=0;i<a.length;i++) {
+		 System.out.println(a[i]);
+		 
+	 }
+ }
+}
